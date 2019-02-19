ecf6849baefdfe9827923e798446e370c5993a3f
stevojed <jedi1014@comcast.net>
2019-02-19 15:11:02 -0500

    Databases

---
+++ Databasing/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Databasing.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+    }
+}
